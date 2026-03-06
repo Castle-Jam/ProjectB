4ff941a48c02a5907624107c5bd126d1f03d0697
Seli <manuel.manu3744@gmail.com>
2026-03-07 00:00:03 +0100

    cheese finish counter added

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -9,18 +9,18 @@ public class Pointer : MonoBehaviour
     private bool movingRight = true;
     [SerializeField] private InputActionReference interaction;
     private bool active = true;
+    private int finishedCheese = 0;
 
 
     void Update()
     {
         if (!active) return;
         float move = speed * Time.deltaTime;
-        
+
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            Debug.Log("passiert");
             Interacted();
-            
+
         }
 
         if (movingRight)
@@ -32,12 +32,11 @@ public class Pointer : MonoBehaviour
             movingRight = false;
         if (pointer.anchoredPosition.x < -560)
             movingRight = true;
-        
+
     }
-    
+
     public void Interacted()
     {
-        active = false;
         Vector2 pointerPos = transform.position;
         float pointerPosX = pointerPos.x;
         float greenBarLeftOne = 854.17f;
@@ -46,22 +45,34 @@ public class Pointer : MonoBehaviour
         float greenBarRightTwo = 1060f;
         float redBarLeft = 998.9f;
         float redBarRight = 1003.9f;
-        Debug.Log(pointerPos);
-        if(greenBarLeftOne <= pointerPosX && pointerPosX<= greenBarRightOne)
+
+        Debug.Log(finishedCheese);
+        if (greenBarLeftOne <= pointerPosX && pointerPosX <= greenBarRightOne)
         {
             Debug.Log("grüner Bereich 1 hehe");
+            finishedCheese += 20;
+            Debug.Log($"grün 1 {finishedCheese}");
         }
-        else if(greenBarLeftTwo <= pointerPosX && pointerPosX<= greenBarRightTwo)
+        else if (greenBarLeftTwo <= pointerPosX && pointerPosX <= greenBarRightTwo)
         {
             Debug.Log("grüner Bereich 2 hehe");
+            finishedCheese += 20;
+            Debug.Log($"grün 2 {finishedCheese}");
         }
-        else if(redBarLeft <= pointerPosX && pointerPosX <= redBarRight)
+        else if (redBarLeft <= pointerPosX && pointerPosX <= redBarRight)
         {
             Debug.Log("roter Bereich");
+            finishedCheese += 40;
+            Debug.Log($"rot {finishedCheese}");
         }
-        else if(pointerPosX < greenBarLeftOne || greenBarRightTwo > pointerPosX)
+        else if (pointerPosX < greenBarLeftOne || greenBarRightTwo > pointerPosX)
         {
             Debug.Log("verkackt");
+        }
+        if (finishedCheese == 100)
+        {
+            active = false;
+            Debug.Log("Cheese finished");
         }
     }
 }
