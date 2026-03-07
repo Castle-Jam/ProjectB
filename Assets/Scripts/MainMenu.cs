6cb3e18e9b3f9edf1adad4acd2f5d7bb7724981b
skribie <vikasyb@icloud.com>
2026-03-07 23:09:05 +0100

    Credits in Main Menu, neues Script, Credits mehr wholesome, unnoetige Script und Scene raus

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject Canvas;
+    [SerializeField] GameObject creditsCanvas;
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(1);
     }
 
+    public void ShowCredits()
+    {
+        Canvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
 
+    public void BackToMenu()
+    {
+        Canvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("spiel aus meow");
+        Application.Quit();
+    }
 }
