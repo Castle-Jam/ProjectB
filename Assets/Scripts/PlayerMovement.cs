5baeb593790fedbcd8ecef257f9315856c6cd198
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2026-03-07 01:27:26 +0100

    Adding a State-Machine and Dash-Implementatio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,29 +7,48 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed = 50f;
     [SerializeField] float rotationSpeed = 5f;
 
+    enum PlayerState { IDLE, WALKING, SPRINTING, INTERACTING}
+    private PlayerState playerState;
+
     private Rigidbody _rigidbody;
     private Rigidbody playerBody;
     private Vector2 moveInput;
+    private bool dashInput;
+
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+
     }
 
-    public void Movement(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
 
     void FixedUpdate()
     {
-        // --- Movement ---
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        _rigidbody.AddForce(move * speed, ForceMode.Acceleration);
+
     }
 
     void Update()
     {
+        
+        switch (playerState)
+        {
+            case PlayerState.IDLE:
+                if (moveInput.sqrMagnitude < 0.01f) Idle();
+                break;
+            case PlayerState.WALKING:
+                HandleWalking();
+                break;
+            case PlayerState.SPRINTING:
+                HandleDash();
+                break;
+            case PlayerState.INTERACTING:
+                break;
+        }
+
+
+
+        // --- Rotation ---
         Vector3 velocity = _rigidbody.linearVelocity;
         velocity.y = 0;
 
@@ -42,5 +62,45 @@ public class PlayerMovement : MonoBehaviour
                 rotationSpeed * Time.deltaTime
             );
         }
+    }
+
+
+
+    public void Walk(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        if (moveInput.sqrMagnitude > 0.01f)
+            playerState = PlayerState.WALKING;
+        else
+            playerState = PlayerState.IDLE;
+    }
+
+    public void HandleWalking()
+    {
+        // --- Movement ---
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        _rigidbody.AddForce(move * speed, ForceMode.Acceleration);
+    }
+    
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            playerState = PlayerState.SPRINTING;
+
+        if (context.canceled)
+            playerState = moveInput.sqrMagnitude > 0.01f ? PlayerState.WALKING : PlayerState.IDLE;
+    }
+
+    private void HandleDash()
+    {
+        Debug.Log("Dashing");
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        _rigidbody.AddForce(move * speed * 2f, ForceMode.Acceleration);
+    }
+
+    public void Idle()
+    {
+        //Debug.Log("Idling");
     }
 }
